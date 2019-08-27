9e86ff8f34e457b114c4944eb91290f38c631433
alfiaqil <alfiaqil@yahoo.com>
2019-08-27 17:44:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(201);
+});
+
 // Simple in-memory store for now
 var dreams = [
   "Find and count some sheep",
