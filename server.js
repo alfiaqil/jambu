dee8fa7a55f941af6cbf58eebca9064fd4294b69
alfiaqil <alfiaqil@yahoo.com>
2019-08-28 08:43:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.post('/db', async (req, res) => {
   })
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
